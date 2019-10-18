ff4eebdbdf30740bfe360b17f8a9752afd0cf249
olezhikz <boxmilo@gmail.com>
2019-10-18 17:32:35 +0600

    init

---
+++ src/Request.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * OLeg Demidov
+ * ------------------------------------------------------
+ * Contact e-mail: end-fin@yandex.ru
+ *
+ * GNU General Public License, version 2:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * ------------------------------------------------------
+ *
+ * @link https://vk.com/u_demidova
+ * @author Oleg Demidov <end-fin@yandex.ru>
+ *
+ */
+
+namespace LS\HTTP;
+
+/**
+ * Description of Request
+ *
+ * @author oleg
+ */
+class Request extends \Nyholm\Psr7\ServerRequest{
+    //put your code here
+}
